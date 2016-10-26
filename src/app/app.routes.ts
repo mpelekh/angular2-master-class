b6834030ad878b7f15c89aa8c0885e82236b153e
Jan Uhlmann <jan.uhlmann@kiwigrid.com>
2016-10-26 14:36:55 +0200

    added type annotation for ContactsAppRoutes constant

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { ContactsListComponent } from './contacts-list/contacts-list.component';
+import { Routes } from '@angular/router';
 
-export const ContactsAppRoutes = [
+export const ContactsAppRoutes: Routes = [
   { path: '', component: ContactsListComponent }
 ];
